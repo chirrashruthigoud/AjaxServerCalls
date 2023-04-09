f399356d822147b2a831ad359654b25d648611d0
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-09 12:27:18 +0530

    ajax async nature call back

--- timer.js
+++ timer.js
@@ -1,13 +1,13 @@
 function showTime(){
     const date = new Date();
-    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "secs";
+    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Secs";
 }
 
 function showSessionExpire(){
     console.log("Activity-B: Your session expire at " + showTime());
 }
 
-console.log("Activity-A Triggering Activity-B at  "+ showTime());
+console.log("Activity-A:Triggering Activity-B at  "+ showTime());
 
 setTimeout(showSessionExpire,5000);
-console.log("Activity-A Triggering Activity-B at  "+ showTime() + "Will expire after 5secs");
+console.log("Activity-A:Triggering Activity-B at  "+ showTime() + "Will expire after 5secs");
